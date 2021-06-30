d3ad5414d495c1e696e662b82be46b0fcf3f4d32
Mannan Shukla <58566668+mannanshukla@users.noreply.github.com>
2021-06-30 11:04:47 -0400

    Add files via upload

---
+++ base.js
@@ -0,0 +1,27 @@
+$(() => {
+
+  $('head')[0].innerHTML = `
+<meta charset="utf-8">
+<title>Robbinsville Tutoring</title>
+<link rel="stylesheet" href="styles.css" type="text/css" media="screen" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  ` + $('head')[0].innerHTML;
+
+  $('body')[0].innerHTML = `
+<div id="nav">
+  <h1>Robbinsville Tutoring</h1>
+  <nav>
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li>|</li>
+      <li><a href="schedule.html">Schedule</a></li>
+      <li>|</li>
+      <li><a href="resources.html">Resources</a></li>
+      <li>|</li>
+      <li><a href="contact.html">Contact & Signup</a></li>
+    </ul>
+  </nav>
+</div>
+  ` + $('body')[0].innerHTML;
+
+});
