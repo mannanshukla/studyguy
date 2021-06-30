d24d668df48bd41695e4a72cb1a1795d39ada003
monado <monado@snopyta.org>
2021-06-30 11:29:23 -0400

    changes

--- base.js
+++ base.js
@@ -2,14 +2,14 @@ $(() => {
 
   $('head')[0].innerHTML = `
 <meta charset="utf-8">
-<title>Robbinsville Tutoring</title>
+<title>Study Guy Tutoring</title>
 <link rel="stylesheet" href="styles.css" type="text/css" media="screen" />
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   ` + $('head')[0].innerHTML;
 
   $('body')[0].innerHTML = `
 <div id="nav">
-  <h1>Robbinsville Tutoring</h1>
+  <h1>Study Guy Tutoring</h1>
   <nav>
     <ul>
       <li><a href="index.html">Home</a></li>
@@ -18,7 +18,7 @@ $(() => {
       <li>|</li>
       <li><a href="resources.html">Resources</a></li>
       <li>|</li>
-      <li><a href="contact.html">Contact & Signup</a></li>
+      <li><a href="contact.html">Contact</a></li>
     </ul>
   </nav>
 </div>
